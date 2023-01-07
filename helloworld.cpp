850422a8daae38639f0eda04a3287f5d4e4424a1
dariusr <darius.r111@gmail.com>
2023-01-07 20:14:46 +0200

    sintax update

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+/* My first program in C++
+   with more comments - block comment */
+
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    //std-"standard" (namespace/library), cout-"character output" (pc display)
+    std::cout << "Hello World!" << std::endl; // << std::endl manipulator put a line break
+    std::cout << "I'm a C++ program"; // just semicolon ok
 }
+
+
+
+
+
+
+  
+
+
+
---
+++ helloworld2.cpp
@@ -0,0 +1,9 @@
+// my second program in C++
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout << "Hello World!\n"; // \n ("\"-escape "n"-new line character)
+  cout << "I'm a C++ program";
+}
